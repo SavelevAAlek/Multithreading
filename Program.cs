0d482740e0a52b5bac2973bcc2d1279f60396f80
Alexey Savelyev <savelyev.main@gmail.com>
2024-01-29 20:27:40 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        Task task1 = PrintMessages("Задача 1", 200);
+        Task task2 = PrintMessages("Задача 2", 150);
+        Task task3 = PrintMessages("Задача 3", 1000);
+
+        await Task.WhenAll(task1, task2, task3);
+
+        Console.WriteLine("Все задачи завершены.");
+    }
+
+    static async Task PrintMessages(string taskName, int delay)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            await Task.Delay(delay);
+            Console.WriteLine($"Сообщение от {taskName}, шаг {i} - Поток: {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
